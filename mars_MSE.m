d5cfd4ac0107243c2dc1547ff90eb1aba73fc708
Will Eaton <43998845+willeaton101@users.noreply.github.com>
2021-07-07 09:29:55 +0100

    Update mars_MSE.m

--- mars_MSE.m
+++ mars_MSE.m
@@ -8,7 +8,7 @@ function [] = mars_MSE(r, tau, scale, input_path, output_folder, output_file)
     % Input parameters: 
     % r [float]           MSE Threshold value e.g. 0.1
     % tau [int]           Coarse graining decimation value e.g. 30 
-    % scale [int]       Max. scale for MSE calculations e.g. 50
+    % scale [int]         Max. scale for MSE calculations e.g. 50
     % input_path [str]    Input file path and name e.g. 'example_folder/file.ascii'
     % output_folder [str] Output folder path  e.g. 'example_folder'
     % output_file [str]   Output file name  e.g. 'example_file.ascii'
@@ -29,7 +29,7 @@ function [] = mars_MSE(r, tau, scale, input_path, output_folder, output_file)
 	end 
     
   
-  	% ______________________________________________________________
+  % ______________________________________________________________
   
     % Create output file file:
     mkdir(output_folder)
@@ -39,6 +39,7 @@ function [] = mars_MSE(r, tau, scale, input_path, output_folder, output_file)
 
 
     % Append filetype: 
+    % This needs more flexibility in input names
     input_path = strcat(input_path, '.ascii');
     
     % Verbose updates
@@ -54,7 +55,7 @@ function [] = mars_MSE(r, tau, scale, input_path, output_folder, output_file)
     d_dim = size(data);
     
     
-    no_channels = min(d_dim); % Assuming that there is more than 3 data points for each time series 
+    no_channels = min(d_dim); % Assuming that there are more data points in time series than channels
     % Transpose data if is [time series, channels] instead of [channels, time series]
     if no_channels == d_dim(2);
         data = transpose(data);
@@ -63,25 +64,23 @@ function [] = mars_MSE(r, tau, scale, input_path, output_folder, output_file)
     % Initialise entropy matrix (MSE output matrix)
     entropy = zeros(no_channels, scale);
     
-    
     for ch = 1:no_channels;
-        temp_data = data(ch, :);
-        
-    % NORMALISE DATA 
-    norm_data = temp_data/(max(abs(temp_data)));
+            temp_data = data(ch, :);
 
-    % Coarse grain the time series - this is before any entropy calculation simply to decimate the timeseries for computational efficiency: 
-    disp('Apply Coarse Grain')
-    
-    cg = coarsegrain_notime(norm_data, tau);
-    disp('Coarse Grain completed')
+	    % NORMALISE DATA 
+	    norm_data = temp_data/(max(abs(temp_data)));
 
+	    % Coarse grain the time series - this is before any entropy calculation simply to decimate the timeseries for computational efficiency: 
+	    disp('Apply Coarse Grain')
 
-    % Calculate entropy over different scales using moving-average         
-    entropy(ch, :) = movavg_mse(cg, r, scale, 'yes');
+	    cg = coarsegrain_notime(norm_data, tau);
+	    disp('Coarse Grain completed')
 
-    disp(strcat('Completed channel ',' ', num2str(ch), '/', num2str(no_channels)))
-       
+	    % Calculate entropy over different scales using moving-average         
+	    entropy(ch, :) = movavg_mse(cg, r, scale, 'yes');
+
+	    disp(strcat('Completed channel ',' ', num2str(ch), '/', num2str(no_channels)))
+
     end
     
     

94e54198a40b0e9e1b1670176f8d4521d586eba0
Will Eaton <43998845+willeaton101@users.noreply.github.com>
2021-07-07 09:15:47 +0100

    Commented

--- SampEn.m
+++ SampEn.m
@@ -1,14 +1,17 @@
 %this function computes the sample entropy for the given data vector 
 %with r tolerance and delay
+
 %input: data: moving average of the data points
 %          r: tolerance for similarity 
 %      delay: shift(1:scale), more shift for higher scale
 %output: entropy
+
+
 function entropy = SampEn(data,r,delay);
 n = length(data);
 nm   = 0;   %number of matched samples of dimension m
 nmp1 = 0;   %number of matched samples of dimension m+1
-%delay = 2;
+
 m = 2;
 for i = 1:n-(m+1)*delay;
   for j = i+delay:1:n-m*delay;
@@ -20,6 +23,7 @@ for i = 1:n-(m+1)*delay;
     end
   end
 end
+
 %------------------------------------------------------------------------
 %nmp1 and nm can be scaled by (n-m+1) and (n-m), but that does not matter 
 %as we scale nmp1 and nm both by the same factor.
